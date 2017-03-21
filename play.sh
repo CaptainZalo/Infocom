9c433846ffa0722b7a620007b22f7c2a2e839aa4
CaptainZalo <captzalo@gmail.com>
2017-03-21 19:54:53 +0100

    Add files via upload

---
+++ play.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd ~/Infocom
+folderpath=$(ls)
+system=$(eval zenity --list --column System $folderpath --width=480 --height=272)
+if [ "$?" -eq 1 ]; then
+   echo "Cancelled."
+else
+   cd $system
+   files=$(ls -Q)
+   rom=$(eval zenity --list --column "Roms" $files --width=480 --height=272)
+   if [ "$?" -eq 1 ]; then
+      echo "Cancelled."
+   else
+      frotz "$rom"
+   fi
+fi
+
