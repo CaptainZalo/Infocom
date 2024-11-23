87c0ee7a5601baed12a9cefffd1090e60791de82
CaptainZalo <captzalo@gmail.com>
2024-11-23 11:27:57 +0100

    Update install.sh
    Fixed a typo. Added string identifiers to play.sh path.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo Install dependencies
 sudo apt-get install frotz zenity -y
-echo Run Zenity frontend with command ~/Infocom/play.sh and choose a game to play.
+echo Run Zenity frontend with command '~/Infocom/play.sh' and choose a game to play.
 echo Profit!
